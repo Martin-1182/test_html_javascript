4991fc92435700458fa4cba43ea9b3bb84959bec
Martin Halaj <m.halaj@websystem.sk>
2021-01-26 09:37:35 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const singleButton = document.getElementById('single')
+const comboButton = document.getElementById('combo')
+const tdValue = document.getElementsByTagName('td')
+const trValue = document.getElementsByTagName('tr')
+
+singleButton.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    let words = Array.from(tdValue)
+
+    let result = words.filter((word) => word.innerText === 'b')
+    result.forEach((element) => {
+        element.classList.add('to-hide')
+    })
+})
+
+comboButton.addEventListener('click', function (event) {
+    event.preventDefault()
+
+    let tRows = Array.from(trValue)
+    let comboResult = tRows.filter((row) => row.className === 'combo')
+
+    comboResult.forEach((row) => {
+        if (row.innerText.length < 3) {
+            console.error('a & b expression not found')
+            return false
+        } else {
+            row.classList.add('to-hide')
+            return true
+        }
+    })
+})
